ca1c63cbc1a3835a637f05e08c7c47953fd68568
naren-ngk <narensakthiit@gmail.com>
2024-01-06 23:10:27 +0530

    Updated sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sponsors from './components/Sponsors/Sponsors';
 import Hero from './components/Hero/Hero';
-import Developers from './components/Developers/Developer'
+import Developers from './components/Developers/Developer';
+import Abacus24 from './components/Abacus/Abacus24';
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Abacus24 />
       <Sponsors />
       <Developers />
     </div>
